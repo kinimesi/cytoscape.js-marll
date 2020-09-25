ea971845856fd5623d8c59589479061d3a9ebd21
kinimesi <kinimesi@users.noreply.github.com>
2020-09-24 23:49:58 -0600

    implements new MARL layouts

--- src/layout/FDLayout.js
+++ src/layout/FDLayout.js
@@ -6,71 +6,192 @@ class FDLayout {
 		this.displacementThresholdPerNode = (1.0 * options.idealEdgeLength) / 100;
 		this.totalDisplacementThreshold =
 			this.displacementThresholdPerNode * options.nodes.length;
+		this.length = this.options.nodes.length;
+		this.nodes = this.options.nodes;
+		this.repulsionConstant = this.options.repulsionConstant;
+		this.springConstant = this.options.springConstant;
+		this.idealEdgeLength = this.options.idealEdgeLength;
+		this.maxDistance = this.options.maxDistance;
+		this.allPairsShortestPath = this.options.allPairsShortestPath;
 	}
 
-	calcSpringForce(edge) {
-		var sourceNode = edge.source();
-		var targetNode = edge.target();
-		var sourcePos = sourceNode.scratch('marll')
-		var targetPos = targetNode.scratch('marll')
+	calcSpringForce(edge, nodeID) {
+		var sourceNodeData = edge.source();
+		var targetNodeData = edge.target();
 
-		var lengthX = targetPos.x - sourcePos.x;
-		var lengthY = targetPos.y - sourcePos.y;
-		var springForce;
-		var springForceX;
-		var springForceY;
+		var lengthX = targetNodeData.x - sourceNodeData.x;
+		var lengthY = targetNodeData.y - sourceNodeData.y;
 
 
-		length = Math.sqrt(lengthX * lengthX + lengthY * lengthY);
+		var length = Math.sqrt(lengthX * lengthX + lengthY * lengthY);
 
 		if(length == 0)
 			return;
 
 		// Calculate spring forces
-		springForce = this.options.springConstant * (length - this.options.idealEdgeLength);
+		var springForce = this.springConstant * (length - this.idealEdgeLength);
 
 		// Project force onto x and y axes
-		springForceX = springForce * (lengthX / length);
-		springForceY = springForce * (lengthY / length);
+		var springForceX = springForce * (lengthX / length);
+		var springForceY = springForce * (lengthY / length);
 
 		// Apply forces on the end nodes
-		sourceNode.scratch('marll').springForceX += springForceX;
-		sourceNode.scratch('marll').springForceY += springForceY;
-		targetNode.scratch('marll').springForceX -= springForceX;
-		targetNode.scratch('marll').springForceY -= springForceY;
+		if (sourceNodeData.ID == nodeID) {
+			sourceNodeData.springForceX += springForceX;
+			sourceNodeData.springForceY += springForceY;
+		} else {
+			targetNodeData.springForceX -= springForceX;
+			targetNodeData.springForceY -= springForceY;
+		}
 	};
 
-	calcSpringForces() {
+	calcSpringForces(n) {
+		var edges = n.edges;
+		var nodeID = n.ID;
 		var edge;
 
-		for (var i = 0; i < this.options.edges.length; i++)
+		for (var i = 0; i < edges.length; i++)
 		{
-			edge = this.options.edges[i];
+			edge = edges[i];
 
-			this.calcSpringForce(edge);
+			this.calcSpringForce(edge, nodeID);
 		}
 	}
 
 	calcRepulsionForce(nodeA, nodeB) {
-		var distanceX = nodeB.scratch('marll').x - nodeA.scratch('marll').x
-		var distanceY = nodeB.scratch('marll').y - nodeA.scratch('marll').y
+		var distanceX = nodeB.x - nodeA.x
+		var distanceY = nodeB.y - nodeA.y
 		var distanceSquared = distanceX * distanceX + distanceY * distanceY;
 		var distance = Math.sqrt(distanceSquared);
+		if (distance == 0 || distance > 100)
+			return;
 
-		var repulsionForce = this.options.repulsionConstant / distanceSquared;
+		var repulsionForce = this.repulsionConstant / distanceSquared;
 
 		// Project force onto x and y axes
 		var repulsionForceX = repulsionForce * distanceX / distance;
 		var repulsionForceY = repulsionForce * distanceY / distance;
 
 		// Apply forces on the two nodes
-		nodeA.scratch('marll').repulsionForceX -= repulsionForceX;
-		nodeA.scratch('marll').repulsionForceY -= repulsionForceY;
-		nodeB.scratch('marll').repulsionForceX += repulsionForceX;
-		nodeB.scratch('marll').repulsionForceY += repulsionForceY;
+		nodeA.repulsionForceX -= repulsionForceX;
+		nodeA.repulsionForceY -= repulsionForceY;
+		//nodeB.repulsionForceX += repulsionForceX;
+		//nodeB.repulsionForceY += repulsionForceY;
 	}
 
-	calcRepulsionForces() {
+	calcRepulsionForces(n) {
+		//for (let i = 0; i < this.options.nodes.length; i++)
+		{
+			let nodeA = n//this.options.nodes[i];
+
+			for (let j = 0; j < this.length; j++)
+			{
+				let nodeB = this.nodes[j];
+				this.calcRepulsionForce(nodeA, nodeB);
+			}
+		}
+	}
+
+	calcSpringForceFR(edge) {
+		var sourceNodeData = edge.source();
+		var targetNodeData = edge.target();
+
+		var lengthX = targetNodeData.x - sourceNodeData.x;
+		var lengthY = targetNodeData.y - sourceNodeData.y;
+
+
+		var length = Math.sqrt(lengthX * lengthX + lengthY * lengthY);
+
+		if(length == 0)
+			return;
+
+		// Calculate spring forces
+		var springForce = (length - this.idealEdgeLength) ** 2 / this.idealEdgeLength
+
+		// Project force onto x and y axes
+		var springForceX = springForce * (lengthX / length);
+		var springForceY = springForce * (lengthY / length);
+
+		// Apply forces on the end nodes
+		sourceNodeData.springForceX += springForceX;
+		sourceNodeData.springForceY += springForceY;
+		targetNodeData.springForceX -= springForceX;
+		targetNodeData.springForceY -= springForceY;
+	}
+
+	calcSpringForcesFR(n) {
+		var edges = n.edges;
+		var nodeID = n.ID;
+		var edge;
+
+		for (var i = 0; i < edges.length; i++)
+		{
+			edge = edges[i];
+
+			this.calcSpringForceFR(edge, nodeID);
+		}
+	}
+
+	calcRepulsionForceFR(nodeA, nodeB) {
+		var distanceX = nodeB.x - nodeA.x
+		var distanceY = nodeB.y - nodeA.y
+		var distanceSquared = distanceX * distanceX + distanceY * distanceY;
+		var distance = Math.sqrt(distanceSquared);
+		if (distance == 0)
+			return;
+
+		var repulsionForce = this.idealEdgeLength ** 2 / distance;
+
+		// Project force onto x and y axes
+		var repulsionForceX = repulsionForce * distanceX / distance;
+		var repulsionForceY = repulsionForce * distanceY / distance;
+
+		// Apply forces on the two nodes
+		nodeA.repulsionForceX -= repulsionForceX;
+		nodeA.repulsionForceY -= repulsionForceY;
+		nodeB.repulsionForceX += repulsionForceX;
+		nodeB.repulsionForceY += repulsionForceY;
+	}
+
+	calcRepulsionForcesFR(n) {
+		//for (let i = 0; i < this.options.nodes.length; i++)
+		{
+			let nodeA = n//this.options.nodes[i];
+
+			for (let j = 0; j < this.length; j++)
+			{
+				let nodeB = this.nodes[j];
+				this.calcRepulsionForceFR(nodeA, nodeB);
+			}
+		}
+	}
+
+	calcStressPerNode(nodeA, nodeB) {
+		var stress = 0;
+		var distanceX = nodeB.x - nodeA.x
+		var distanceY = nodeB.y - nodeA.y
+		var distanceSquared = distanceX * distanceX + distanceY * distanceY;
+		var distance = Math.sqrt(distanceSquared);
+
+		var theoricDistance = this.allPairsShortestPath[nodeA.ID][nodeB.ID];
+		if (theoricDistance && theoricDistance <= this.maxDistance) {
+			stress = 1/theoricDistance * (distance - theoricDistance * this.idealEdgeLength) ** 2;
+		}
+
+		return stress;
+	}
+
+	calcLocalStress(node) {
+		var stress = 0;
+		for (let i = 0; i < this.length; i++) {
+			let nodeB = this.nodes[i];
+			stress += this.calcStressPerNode(node, nodeB);
+		}
+		return stress;
+	}
+
+	calcGlobalStress() {
+		var stress = 0;
 		for (let i = 0; i < this.options.nodes.length; i++)
 		{
 			let nodeA = this.options.nodes[i];
@@ -78,17 +199,21 @@ class FDLayout {
 			for (let j = i + 1; j < this.options.nodes.length; j++)
 			{
 				let nodeB = this.options.nodes[j];
-				this.calcRepulsionForce(nodeA, nodeB);
+				stress += this.calcStressPerNode(nodeA, nodeB);
 			}
 		}
+		return stress;
 	}
 
 	isConverged() {
 		var oscilating = Math.abs(this.totalDisplacement - this.oldTotalDisplacement) < 2;
 		var converged = this.totalDisplacement < this.totalDisplacementThreshold;
+		var stressConverged = Math.abs(this.newStress - this.oldStress) / this.oldStress < 0.0001;
 
 		this.oldTotalDisplacement = this.totalDisplacement;
+		this.oldStress = this.newStress;
 		this.totalDisplacement = 0;
+		this.newStress = 0;
 		return converged || oscilating;
 	}
 }
--- src/layout/continuous.js
+++ src/layout/continuous.js
@@ -16,19 +16,70 @@ const defaults = {
 	idealEdgeLength: 100,
 	springConstant: 0.2,
 	repulsionConstant: 4500,
+	incremental: false,
+	delta: 10,
 }; // TODO define
 
 
 class Layout extends ContinuousLayout {
 	constructor( options ){
 		super( assign( {}, defaults, options ) );
-		this.FDLayout = new FDLayout(this.state);
 		this.state.delta = 10;
-		console.log( this.state) 
+
+		if (!this.state.maxDistance || options.rewardFunction == 'globalStress') {
+			this.state.maxDistance = this.state.nodes.length;
+		}
+
+		if (this.state.incremental) {
+			this.state.randomize = false;
+		}
+
+		if (!this.state.newNodes) {
+			this.state.newNodes = this.state.cy.collection();
+		}
+
+		if (options.rewardFunction == 'customReward') {
+			this.rewardFunction = (n) => {
+				return 5*this.getEdgeLengthVariance(n) + 10 * this.getNumberOfNodeOverlaps(n) +
+					this.getNumberOfEdgeCrossings(n) + this.getAngleVariance(n) + this.getNodeDistanceVariance(n);
+			}
+		} else if (options.rewardFunction == 'localStress') {
+			var shortestPaths = this.state.eles.floydWarshall().distance;
+			this.state.allPairsShortestPath = {};
+			for (let i = 0; i< this.state.nodes.length; i++ ) {
+					var nodeA = this.state.nodes[i];
+					this.state.allPairsShortestPath[nodeA.id()] = {};
+				for (let j = 0; j< this.state.nodes.length; j++ ) {
+					var nodeB = this.state.nodes[j];
+					this.state.allPairsShortestPath[nodeA.id()][nodeB.id()] = shortestPaths(nodeA, nodeB);
+				}
+			}
+			this.rewardFunction = this.getCurrentLocalStress;
+		} else if (options.rewardFunction == 'globalStress') {
+			this.state.allPairsShortestPath = this.state.eles.floydWarshall().distance;
+			this.rewardFunction = this.getCurrentGlobalStress;
+		} else if (options.rewardFunction == 'forceDirectedFR') {
+			this.rewardFunction = this.getCurrentTotalForcesFR;
+		} else if (options.rewardFunction == 'hybrid') {
+			var shortestPaths = this.state.eles.floydWarshall().distance;
+			this.state.allPairsShortestPath = {};
+			for (let i = 0; i< this.state.nodes.length; i++ ) {
+					var nodeA = this.state.nodes[i];
+					this.state.allPairsShortestPath[nodeA.id()] = {};
+				for (let j = 0; j< this.state.nodes.length; j++ ) {
+					var nodeB = this.state.nodes[j];
+					this.state.allPairsShortestPath[nodeA.id()][nodeB.id()] = shortestPaths(nodeA, nodeB);
+				}
+			}
+			this.rewardFunction = (n) => this.getCurrentTotalForcesFR(n) + this.getCurrentLocalStress(n);
+		} else {
+			this.rewardFunction = this.getCurrentTotalForces;
+		}
+
+		this.FDLayout = new FDLayout(this.state);
 	}
 
-	initAgent(n) {
-		let scratch = this.getScratch( n ); // per-element layout data/state, x/y, etc.
+	initAgent(n, scratch) {
 		var env = {};
 		env.getNumStates = function() { return 9; }
 		env.getMaxNumActions = function() { return 9; }
@@ -41,7 +92,7 @@ class Layout extends ContinuousLayout {
 		spec.alpha = 0.1; // value function learning rate
 		spec.lambda = 0.1; // eligibility trace decay, [0,1). 0 = no eligibility traces
 		spec.replacing_traces = false; // use replacing or accumulating traces
-		spec.planN = 50; // number of planning steps per iteration. 0 = no planning
+		spec.planN = 20; // number of planning steps per iteration. 0 = no planning
 
 		spec.smooth_policy_update = false; // non-standard, updates policy smoothly to follow max_a Q
 		spec.beta = 0.3; // learning rate for smooth policy update
@@ -51,6 +102,198 @@ class Layout extends ContinuousLayout {
 		scratch.state = 4;
 		scratch.agent.counter = 0;
 		scratch.oldTotalForce = null;
+
+	}
+
+	getCurrentLocalStress(n) {
+		return this.FDLayout.calcLocalStress(n);
+	}
+
+	getCurrentGlobalStress() {
+		return this.FDLayout.calcGlobalStress();
+	}
+
+	getCurrentTotalForces(node) {
+		//this.state.nodes.forEach(n => {
+			node.springForceX = 0;
+			node.springForceY = 0;
+			node.repulsionForceX = 0;
+			node.repulsionForceY = 0;
+		//})
+		this.FDLayout.calcSpringForces(node);
+		this.FDLayout.calcRepulsionForces(node);
+		var fx = node.springForceX + node.repulsionForceX;
+		var fy = node.springForceY + node.repulsionForceY;
+		var totalForce = Math.sqrt(fx*fx + fy*fy);
+		return totalForce;
+	}
+
+	getCurrentTotalForcesFR(n) {
+		let scratch = n;
+		scratch.springForceX = 0;
+		scratch.springForceY = 0;
+		scratch.repulsionForceX = 0;
+		scratch.repulsionForceY = 0;
+		this.FDLayout.calcSpringForcesFR(n);
+		this.FDLayout.calcRepulsionForcesFR(n);
+		var fx = scratch.springForceX + scratch.repulsionForceX;
+		var fy = scratch.springForceY + scratch.repulsionForceY;
+		var totalForce = Math.sqrt(fx*fx + fy*fy);
+		return totalForce;
+	}
+
+	getNumberOfNodeOverlaps(node) {
+		let state = this.state;
+		let boundingBox = function(node) {
+			let scratch = node;
+			return {x1: scratch.x - scratch.wHalf, y1: scratch.y - scratch.hHalf,
+				x2: scratch.x + scratch.wHalf, y2: scratch.y + scratch.hHalf};
+		}
+
+		let doesOverlap = function(node, otherNode) {
+			let bb = boundingBox(node), bbOther = boundingBox(otherNode);
+			return !(bbOther.x1 > bb.x2 || bbOther.x2 < bb.x1 || bbOther.y1 > bb.y2 || bbOther.y2 < bb.y1);
+		};
+
+		let overlaps = -1;
+		//let nodeArray = this.state.cy.nodes().toArray();
+
+		//for (let i = 0; i < nodeArray.length; i++) {
+		this.state.nodes.forEach(otherNode => {
+			if (doesOverlap(node, otherNode)) {
+				overlaps++;
+			}
+		});
+		return overlaps;
+	}
+
+	getNumberOfEdgeCrossings(n) {
+		let doesIntersect = function(a,b,c,d,p,q,r,s) {
+			var det, gamma, lambda;
+			det = (c - a) * (s - q) - (r - p) * (d - b);
+			if (det === 0) {
+				return false;
+			} else {
+				lambda = ((s - q) * (r - a) + (p - r) * (s - b)) / det;
+				gamma = ((b - d) * (r - a) + (c - a) * (s - b)) / det;
+				return (0.01 < lambda && lambda < 0.99) && (0.1 < gamma && gamma < 0.99);
+			}
+		};
+
+		let crosses = 0;
+		var edges = n.edges;
+		let edgeArray = this.state.edges;
+
+		for (let i = 0; i < edges.length; i++) {
+			var p = edges[i].source();
+			var q = edges[i].target();
+			for (var j = i + 1; j < edgeArray.length; j++) {
+				var r = edgeArray[j].source();
+				var s = edgeArray[j].target();
+				if (doesIntersect(p.x, p.y, q.x, q.y, r.x, r.y, s.x, s.y)) {
+					crosses++;
+				}
+			}
+		}
+		return crosses;
+	}
+
+	getTotalArea(cy) {
+		let bb = this.state.nodes.boundingBox();
+		return bb.w * bb.h;
+	}
+
+	calcDegree(node, center) {
+		let nScrath = node;
+		let cScrath = center;
+		return Math.atan2(nScrath.y - cScrath.y, nScrath.x - cScrath.x);
+	}
+
+	calcEdgeLengthVariance(edge) {
+		var sourcePos = edge.source();
+		var targetPos = edge.target();
+
+		var lengthX = targetPos.x - sourcePos.x;
+		var lengthY = targetPos.y - sourcePos.y;
+
+		length = Math.sqrt(lengthX * lengthX + lengthY * lengthY);
+
+
+		// Calculate variance
+		return ((length - this.options.idealEdgeLength)/this.options.idealEdgeLength) ** 2;
+	}
+
+	calcNodeDistanceVariance(sourceNode, targetNode) {
+		var sourcePos = sourceNode;
+		var targetPos = targetNode;
+
+		var lengthX = targetPos.x - sourcePos.x;
+		var lengthY = targetPos.y - sourcePos.y;
+		var springForce;
+		var springForceX;
+		var springForceY;
+
+
+		length = Math.sqrt(lengthX * lengthX + lengthY * lengthY);
+
+		if (length > this.options.idealEdgeLength / 4) {
+			return 0;
+		}
+
+		// Calculate variance
+		return Math.abs((length - this.options.idealEdgeLength))
+	}
+
+	getNodeDistanceVariance(n) {
+		var variance = 0;
+		//for (var i = 0; i < this.state.nodes.length; i++) {
+			let nodeA = n;//this.state.nodes[i];
+			for (let j = 0; j < this.state.nodes.length; j++)
+			{
+				let nodeB = this.state.nodes[j];
+				variance += this.calcNodeDistanceVariance(nodeA, nodeB)
+			}
+		//}
+		//console.log( 'var: ' +variance) 
+		return variance;
+	}
+
+	getEdgeLengthVariance(n) {
+		var edges = n.edges;
+		var variance = 0;
+		for (var i = 0; i < edges.length; i++) {
+			let edge = edges[i]
+			variance += this.calcEdgeLengthVariance(edge);
+		}
+		return variance / edges.length;
+	}
+
+	calcAngle(n,c,m) {
+		let nScrath = n;
+		let mScrath = m;
+		let cScrath = c;
+		let ndy = nScrath.y - cScrath.y, ndx = nScrath.x - cScrath.x;
+		let mdy = mScrath.y - cScrath.y, mdx = mScrath.x - cScrath.x;
+		let na = Math.atan2(ndy, ndx)
+		let ma =  Math.atan2(mdy, mdx);
+
+		let da = Math.abs(na - ma);
+		da = da < Math.PI ? da : 2 * Math.PI - da;
+
+		return da
+	}
+
+	getAngleVariance(n) {
+		var arr = n.neighbours;//hood('node');
+		arr.sort((a, b) => this.calcDegree(a, n) - this.calcDegree(b, n));
+		var length = arr.length;
+		var variance = 0;
+		for (let i = 0; i < length - 1; i++) {
+			variance += (Math.abs(this.calcAngle(arr[i], n, arr[i+1])) - 2 * Math.PI / length) ** 2;
+		}
+
+		variance += (Math.abs(this.calcAngle(arr[length - 1], n, arr[0])) - 2 * Math.PI / length) ** 2;
+		return variance;
 	}
 
 	takeStep(n) {
@@ -61,58 +304,43 @@ class Layout extends ContinuousLayout {
 		];
 
 		var delta = this.state.delta;
-		this.state.nodes.forEach(n => {
-			let scratch = this.getScratch( n );
-			scratch.springForceX = 0;
-			scratch.springForceY = 0;
-			scratch.repulsionForceX = 0;
-			scratch.repulsionForceY = 0;
-		})
-		this.FDLayout.calcSpringForces();
-		this.FDLayout.calcRepulsionForces();
-		let scratch = this.getScratch( n ); // per-element layout data/state, x/y, etc.
+
+		if (this.state.incremental && !this.state.newNodes.has(n)) {
+			delta /= 10;
+		}
+
+		var totalForce = this.rewardFunction(n);
+		let scratch = n;//this.getScratch( n ); // per-element layout data/state, x/y, etc.
 		var action = scratch.agent.act(scratch.state);
-		var fx = scratch.springForceX + scratch.repulsionForceX;
-		var fy = scratch.springForceY + scratch.repulsionForceY;
 		var displacementX = displacement[action][0];
 		var displacementY = displacement[action][1];
-		var totalForce = Math.sqrt(fx*fx + fy*fy);
 		scratch.x += displacement[action][0] * delta;
 		scratch.y += displacement[action][1] * delta;
 		var reward = 0;
+		this.FDLayout.totalDisplacement += Math.abs(displacementX) + Math.abs(displacementY);
 
-		this.state.nodes.forEach(n => {
-			let scratch = this.getScratch( n );
-			scratch.springForceX = 0;
-			scratch.springForceY = 0;
-			scratch.repulsionForceX = 0;
-			scratch.repulsionForceY = 0;
-		})
-
-		this.FDLayout.calcSpringForces();
-		this.FDLayout.calcRepulsionForces();
-		fx = scratch.springForceX + scratch.repulsionForceX;
-		fy = scratch.springForceY + scratch.repulsionForceY;
-		var newtotalForce = Math.sqrt(fx*fx + fy*fy);
+		var newtotalForce = this.rewardFunction(n);
 
 		if (scratch.oldTotalForce === null) {
-			console.log( 'resetting') 
 			reward = 1;
 		} else {
+			reward = scratch.oldTotalForce - scratch.stress;//totalForce - newtotalForce;//totalForce - scratch.oldTotalForce;
 			reward = totalForce - newtotalForce;//totalForce - scratch.oldTotalForce;
 		}
+		//console.log( reward) 
 
 
-		if (Math.abs(reward < 0.1)) {
-			scratch.x -= displacement[action][0] * delta;
-			scratch.y -= displacement[action][1] * delta;
-		}
 
 
-		//console.log( totalForce +', ' +newtotalForce) 
 		scratch.oldTotalForce = totalForce;
 		scratch.agent.learn(reward, scratch.state, action);
-		scratch.state = action;
+		if (reward < 0 && Math.random() > 0.1) {
+			scratch.x -= displacement[action][0] * delta;
+			scratch.y -= displacement[action][1] * delta;
+			//console.log( 'too small: ' + reward) 
+		} else {
+			scratch.state = action;
+		}
 	}
 
 	prerun(){
@@ -120,24 +348,22 @@ class Layout extends ContinuousLayout {
 
 		// regular nodes
 		state.nodes.forEach( n => {
-			let scratch = this.getScratch( n ); // per-element layout data/state, x/y, etc.
+			let scratch = n//state.layoutData[n.id()];//this.getScratch( n ); // per-element layout data/state, x/y, etc.
 
-			// example of setting per-element state based on an option value/function
-			scratch.foo = optFn( state.foo, n );
-			scratch.springForceX = 0;
-			scratch.springForceY = 0;
-			scratch.repulsionForceX = 0;
-			scratch.repulsionForceY = 0;
+			n.springForceX = 0;
+			n.springForceY = 0;
+			n.repulsionForceX = 0;
+			n.repulsionForceY = 0;
 
-			this.initAgent(n);
-		} );
+			if (this.state.rewardFunction == 'customReward') {
+				n.wHalf = n.outerWidth() / 2;
+				n.hHalf = n.outerHeight() / 2;
+				n.neighbours = n.neighbourhood('node');
+			}
+			n.edges = n.connectedEdges();
+			n.ID = n.id();
 
-		// regular edge springs
-		state.edges.forEach( e => {
-			let scratch = this.getScratch( e ); // per-element layout data/state, x/y, etc.
-
-			// example of setting per-element state based on an option value/function
-			scratch.foo = optFn( state.foo, e );
+			this.initAgent(n, scratch);
 		} );
 	}
 
@@ -146,30 +372,17 @@ class Layout extends ContinuousLayout {
 		let state = this.state;
 		let isDone = true;
 
-		this.FDLayout.calcSpringForces();
-		this.FDLayout.calcRepulsionForces();
 		state.nodes.forEach(n => {
-			let s = this.getScratch(n);
-
-			// example : put node at random position
-			let displacementX = s.springForceX + s.repulsionForceX;
-			let displacementY = s.springForceY + s.repulsionForceY;
-			this.FDLayout.totalDisplacement +=
-				Math.abs(displacementX) + Math.abs(displacementY);
-			//s.x += displacementX;
-			//s.y += displacementY;
 			this.takeStep(n)
 
-
-			s.springForceX = 0;
-			s.springForceY = 0;
-			s.repulsionForceX = 0;
-			s.repulsionForceY = 0;
+			n.springForceX = 0;
+			n.springForceY = 0;
+			n.repulsionForceX = 0;
+			n.repulsionForceY = 0;
 		} );
 
-		if (this.state.currentIteration % 100 == 0) {
-			console.log( this.state.currentIteration) 
-			console.log( this.FDLayout.totalDisplacement) 
+		if (this.state.currentIteration > 0 &&this.state.currentIteration % 800 == 0) {
+		this.state.delta /= 2;
 		}
 		return false//this.FDLayout.isConverged();
 	}
