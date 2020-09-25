11036f0900aef4ea380e04b0f5324dcb7fc6f6b9
kinimesi <kinimesi@users.noreply.github.com>
2020-09-24 23:48:39 -0600

    fixed animation bug and reindent

--- src/layout/continuous-base/tick.js
+++ src/layout/continuous-base/tick.js
@@ -1,41 +1,59 @@
 const nop = function(){};
 
 let tick = function( state ){
-  let s = state;
-  let l = state.layout;
+	let s = state;
+	let l = state.layout;
 
-  let tickIndicatesDone = l.tick( s );
+	let tickIndicatesDone = l.tick( s );
 
-  if( s.firstUpdate ){
-    if( s.animateContinuously ){ // indicate the initial positions have been set
-      s.layout.emit('layoutready');
-    }
-    s.firstUpdate = false;
-  }
+	if( s.firstUpdate ){
+		if( s.animateContinuously ){ // indicate the initial positions have been set
+			s.layout.emit('layoutready');
+		}
+		s.firstUpdate = false;
+	}
 
-  s.tickIndex++;
+	s.tickIndex++;
 
-  let duration = s.startTime - Date.now();
+	let duration = s.startTime - Date.now();
 
-  return !s.infinite && ( tickIndicatesDone || s.tickIndex >= s.maxIterations || duration >= s.maxSimulationTime );
+	return !s.infinite && ( tickIndicatesDone || s.tickIndex >= s.maxIterations || duration >= s.maxSimulationTime );
 };
 
 let multitick = function( state, onNotDone = nop, onDone = nop ){
-  let done = false;
-  let s = state;
+	let done = false;
+	let s = state;
 
-  for( let i = 0; i < s.maxIterations; i++ ){
-    s.currentIteration = i;
-    done = !s.running || tick( s );
+	for( let i = 0; i < s.refresh; i++ ){
+		s.currentIteration = i;
+		done = !s.running || tick( s );
 
-    if( done ){ break; }
-  }
+		if( done ){ break; }
+	}
 
-  if( !done ){
-    onNotDone();
-  } else {
-    onDone();
-  }
+	if( !done ){
+		onNotDone();
+	} else {
+		onDone();
+	}
 };
 
-module.exports = { tick, multitick };
+let alltick = function( state, onNotDone = nop, onDone = nop ){
+	let done = false;
+	let s = state;
+
+	for( let i = 0; i < s.maxIterations; i++ ){
+		s.currentIteration = i;
+		done = !s.running || tick( s );
+
+		if( done ){ break; }
+	}
+
+	if( !done ){
+		onNotDone();
+	} else {
+		onDone();
+	}
+};
+
+module.exports = { tick, multitick , alltick};
